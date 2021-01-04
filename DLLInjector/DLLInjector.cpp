0cf1f617b7705e9be29b448594e9368f6f72028e
E7ite <z7z.e7ite.z7z@gmail.com>
2021-01-04 04:55:36 -0800

    Functionality completed; can load/unload 32-bit DLLs when compiled in 32-bit mode

--- DLLInjector/DLLInjector.cpp
+++ DLLInjector/DLLInjector.cpp
@@ -1,9 +1,13 @@
-// I dont intend for this to be portable, so will use Windows specific protocols
-
-#include <iostream>
-#include <cwchar>
+#include <tchar.h>
+#include <cstdio>
 #include <windows.h>
 #include <tlhelp32.h>
+#include <psapi.h>
+
+// Obtains string literal for current library based on if Unicode is defined
+#define _LOADLIBRARYSTRIMPL(a) #a
+#define LOADLIBRARYSTRIMPL(a) _LOADLIBRARYSTRIMPL(a)
+#define LOADLIBRARYSTR LOADLIBRARYSTRIMPL(LoadLibrary)
 
 /**
  * Reports errors from Win32 API if errStr is specified, frees any handles
@@ -11,8 +15,25 @@
  *
  * @param errStr If not null, a string to output along with the converted error
  * @return -1
-*/
-int ReportError(LPCWSTR errStr);
+ */
+int ReportError(LPCTSTR errStr);
+
+/**
+ * Gets a handle to the loaded DLL module with name Dllname from argument process
+ * @param snap Snapshot of all processes from CreateToolHelp32Snapshot
+ * @param dllName Pointer to null-terminated string with name of DLl to find
+ * @param process THe process ID of which to search through the loaded modules
+ * @return If success, a handle to the loaded DLL. nullptr if not found or error.
+ */
+HMODULE GetDllHandle(LPCTSTR dllName, DWORD processID);
+
+/**
+ * Gets the pid of the process specified by processName
+ * @param snap Snapshot of all processes from CreateToolHelp32Snapshot
+ * @param processName Pointer to null terminated TCHAR string of process name
+ * @return If success, the process ID with name processName. -1 if not found or error
+ */
+DWORD GetProcessID(LPCTSTR processName);
 
 struct HandleManager
 {
@@ -25,11 +46,11 @@ struct HandleManager
 
     ~HandleManager() 
     {
-        if (CloseHandle(handle) == FALSE)
-            ReportError(L"CloseHandle");
+        if (this->handle != nullptr && CloseHandle(this->handle) == FALSE)
+            ReportError(TEXT("CloseHandle"));
     }
 
-    operator HANDLE() { return handle; }
+    operator HANDLE() { return this->handle; }
 };
 
 struct ExternMemManager
@@ -41,37 +62,37 @@ struct ExternMemManager
     {
         this->base= VirtualAllocEx(this->process, nullptr, size, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
         if (this->base == nullptr)
-            ReportError(L"VirtualAllocEx");
+            ReportError(TEXT("VirtualAllocEx"));
 
         if (WriteProcessMemory(this->process, this->base, buffer, size, nullptr) == FALSE)
-            ReportError(L"WriteProcessMemory");
+            ReportError(TEXT("WriteProcessMemory"));
     }
 
     ~ExternMemManager() 
     { 
         if (!VirtualFreeEx(this->process, this->base, 0, MEM_RELEASE))
-            ReportError(L"VirtualFreeEx"); 
+            ReportError(TEXT("VirtualFreeEx")); 
     }
 
-    operator LPVOID() { return base; }
+    operator LPVOID() { return this->base; }
 };
 
-int ReportError(LPCWSTR errStr)
+int ReportError(LPCTSTR errStr)
 {
     // Format the system error message and save pointer to it in errMsg
     if (errStr != nullptr)
     {
-        LPWSTR errMsg;
+        LPTSTR errMsg;
         FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
                       nullptr,
                       GetLastError(),
                       0,
-                      (LPWSTR)&errMsg,
+                      (LPTSTR)&errMsg,
                       0,
                       nullptr);
     
         // Output the error message and free its resources
-        std::wcerr << errStr << ": " << errMsg << std::endl;
+        _tprintf(TEXT("%s: %s"), errStr, errMsg);
         LocalFree(errMsg);
     }
 
@@ -82,82 +103,118 @@ int ReportError(LPCWSTR errStr)
     return -1;
 }
 
-DWORD GetProcessID(LPCWSTR processName)
+DWORD GetProcessID(LPCTSTR processName)
 {
     // Get a snapshot of all the current processes on the system
     HandleManager processesSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (processesSnap == INVALID_HANDLE_VALUE)
-        return ReportError(L"GetProcessID");
+        return ReportError(TEXT("CreateToolhelp32Snapshot"));
 
     // Obtain the first process based on the snapshot
     PROCESSENTRY32 processInfo;
     processInfo.dwSize = sizeof(PROCESSENTRY32);
     if (!Process32First(processesSnap, &processInfo))
-        return ReportError(L"Process32First");
+        return ReportError(TEXT("Process32First"));
 
     // Iterate through all processes and find the one with matching name as arg 
     // without the extension
-    DWORD processID = -1;
     do
-    {
-        if (!wcsncmp(processInfo.szExeFile, processName, wcslen(processInfo.szExeFile) - 3))
-        {
-            processID = processInfo.th32ProcessID;
-            break;
-        }
-    }
+        if (!lstrcmp(processInfo.szExeFile, processName))
+            return processInfo.th32ProcessID;
     while (Process32Next(processesSnap, &processInfo));
 
-    return processID;
+    return -1;
 }
 
-int wmain(int argc, const wchar_t **argv)
+HMODULE GetDllHandle(LPCTSTR dllName, DWORD processID)
+{
+    // Get a snapshot of all the current 32-bit and 64-bit modules on the system 
+    HandleManager modulesSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, processID);
+    if (modulesSnap == INVALID_HANDLE_VALUE)
+    {
+        ReportError(TEXT("CreateToolhelp32Snapshot"));
+        return nullptr;
+    }
+
+    // Obtain the first process based on the snapshot
+    MODULEENTRY32 moduleInfo;
+    moduleInfo.dwSize = sizeof(MODULEENTRY32);
+    if (!Module32First(modulesSnap, &moduleInfo))
+    {
+        ReportError(TEXT("Module32First"));
+        return nullptr;
+    }
+
+    // Iterate through all processes and find the one with matching name as arg 
+    // without the extension
+    do
+        if (!lstrcmp(moduleInfo.szModule, dllName))
+            return moduleInfo.hModule;
+    while (Module32Next(modulesSnap, &moduleInfo));
+}
+
+int _tmain(int argc, const TCHAR **argv)
 {
     // Check command line arguments
     if (argc < 4)
     {
-        std::wcerr << L"Invalid usage. Correct usage:\n" << argv[0] << L" <PROCESS> <load/unload> <DLL>\n";
+        _tprintf(TEXT("Invalid usage. Correct usage:\n%s <PROCESS> <load/unload> <DLL>\n"), argv[0]);
         return ReportError(nullptr);
     }
 
     // Get the full file path to the DLL and size accounting for Unicode + null terminator
-    LPWSTR dllName;
-    WCHAR fullDllPath[MAX_PATH];
-    if (!GetFullPathName(argv[3], sizeof fullDllPath / sizeof WCHAR, fullDllPath, &dllName))
+    LPTSTR dllName;
+    TCHAR fullDllPath[MAX_PATH];
+    if (!GetFullPathName(argv[3], sizeof fullDllPath / sizeof TCHAR, fullDllPath, &dllName))
     {
-        std::wcerr << L"Could not find " << argv[3] << L'\n';
+        _tprintf(TEXT("Could not find %s\n"), argv[3]);
         return ReportError(nullptr);
     }
-    std::wcerr << L"FUll path name: " << fullDllPath << std::endl;
-
-
+    
     // Attempt to locate the process specified and get its ID
-    DWORD processID = GetProcessID(argv[1]);
-    if (processID == -1)
+    DWORD remoteProcessID = GetProcessID(argv[1]);
+    if (remoteProcessID == -1)
     {
-        std::wcerr << L"Could not find process " << argv[1] << L'\n';
+        _tprintf(TEXT("Could not find process %s\n"), argv[1]);
         return ReportError(nullptr);
     }
 
     // Get the handle to the OS process
-    HandleManager remoteProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processID);
+    HandleManager remoteProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | 
+                                              PROCESS_VM_OPERATION | PROCESS_VM_WRITE | 
+                                              PROCESS_VM_READ, 
+                                              FALSE,
+                                              remoteProcessID);
     if (remoteProcess == nullptr)
-        return ReportError(L"OpenProcess");
+        return ReportError(TEXT("OpenProcess"));
 
     // Save the target's DLL path into the target process's address space
     ExternMemManager remoteDllPath(remoteProcess, fullDllPath, sizeof fullDllPath);
 
     // Get the start routine for the thread to execute
+    LPVOID onStartArg = nullptr;
     LPTHREAD_START_ROUTINE onStart = nullptr;
     const char *onStartStr = nullptr;
-    HMODULE kernelDllHandle = GetModuleHandle(L"kernel32");
-    if (!wcsncmp(argv[2], L"load", sizeof L"load"))
-        onStartStr = "LoadLibraryW";
-    else if (!wcsncmp(argv[2], L"unload", sizeof L"unload"))
+    HMODULE kernelDllHandle = GetModuleHandle(TEXT("kernel32"));
+
+    if (!lstrcmp(argv[2], TEXT("load")))
+    {
+        onStartStr = LOADLIBRARYSTR;
+        onStartArg = remoteDllPath.base;
+    }
+    else if (!lstrcmp(argv[2], TEXT("unload")))
+    {
         onStartStr = "FreeLibrary";
+        onStartArg = GetDllHandle(dllName, remoteProcessID);
+        if (onStartArg == nullptr)
+        {
+            _tprintf(TEXT("%s was not loaded in %s\n"), dllName, argv[1]);
+            return ReportError(nullptr);
+        }
+    }
     if (onStartStr == nullptr)
     {
-        std::wcerr << L"Invalid option specified: " << argv[2] << L"\nShould be \"load\" or \"unload\"\n";
+        _tprintf(TEXT("Invalid option specified: %s Should be \"load\" or \"unload\"\n"), argv[2]);
         return ReportError(nullptr);
     }
 
@@ -165,27 +222,30 @@ int wmain(int argc, const wchar_t **argv)
     // kernel32.dll
     onStart = (LPTHREAD_START_ROUTINE)GetProcAddress(kernelDllHandle, onStartStr);
     if (onStart == nullptr)
-        return ReportError(L"GetProcAddress");
+        return ReportError(TEXT("GetProcAddress"));
 
     // Create a thread in target process, have it load the desired DLL
     // using LoadLibrary from kernel32.dll and the allocated file path for
     // the target DLL
-    HandleManager remoteThread = CreateRemoteThread(remoteProcess, nullptr, 0, onStart, remoteDllPath, 0, nullptr);
+    HandleManager remoteThread = CreateRemoteThread(remoteProcess, nullptr, 0, onStart, onStartArg, 0, nullptr);
     if (remoteThread == nullptr)
-        return ReportError(L"CreateRemoteThread");
+        return ReportError(TEXT("CreateRemoteThread"));
 
     // Wait for the thread in the target process to successfully load the dll
     if (WaitForSingleObject(remoteThread, INFINITE) == WAIT_FAILED)
-        return ReportError(L"WaitForSingleObject");
+        return ReportError(TEXT("WaitForSingleObject"));
 
-    // Check result of error code
-    DWORD remoteThreadResult;
-    if (GetExitCodeThread(remoteThread, &remoteThreadResult) == FALSE)
-        return ReportError(L"GetExitCodeThread");
-    if (!remoteThreadResult)
+    // Check result of requested kernel32.dll function. Both functions return 0 on failure.
+    DWORD loadLibraryResult;
+    if (GetExitCodeThread(remoteThread, &loadLibraryResult) == FALSE)
+        return ReportError(TEXT("GetExitCodeThread"));
+    if (!loadLibraryResult)
     {
-        std::cerr << onStartStr;
-        std::wcerr << L" failed to " << argv[2] << L" " << dllName << L'\n';
+#ifdef UNICODE
+        _tprintf(TEXT("%S failed to %s %s\n"), onStartStr, argv[2], dllName);
+#else
+        _tprintf(TEXT("%s failed to %s %s\n"), onStartStr, argv[2], dllName);
+#endif
         return ReportError(nullptr);
     }
 
